ca3674076cb277b6766bb49f723f4f0572572689
pfurio <pedrofurio@gmail.com>
2015-10-06 14:40:16 +0100

    Added new components and functionality.
    * Add configuration file that will be read by the application to load the proper software tool and view.
    * Changed the way the form is shown. The help is shown when hovering the mouse over the form.
    * Folder view is shown when clicking in the folder icon.

--- www/src/conf/config.js
+++ www/src/conf/config.js
@@ -3,9 +3,90 @@
  * Changes to this file may cause the application does not work as it should
  **/
 
+EASYNGS_VERSION = "v0.5";
+
 // Development
 OPENCGA_VERSION = "v1";
 OPENCGA_HOST = "http://bioinfodev.hpc.cam.ac.uk/opencga";
 
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
+
+var QUALITY_CONTROL = {
+    "trimming": [
+        {
+            "name": "fastX",
+            "version": "v1.0",
+            "path": "/opt/etc/fastx",
+            "description": "Toolkit to trim and filter fastq samples."
+        },
+        {
+            "name": "CutAdapt",
+            "version": "v2.1",
+            "path": "/opt",
+            "description": "Tool to remove adapters from fastq files."
+        }
+    ]
+};
+
+var RNASEQ = {
+    "mapping": [
+        {
+            "name": "Tophat",
+            "version": "v2.1",
+            "path": "mypath",
+            "description": "Widely used RNA-seq mapper"
+        },
+        {
+            "name": "STAR",
+            "version": "v12",
+            "path": "asdasd",
+            "description": "STAR works..."
+        }
+    ],
+    "counting": [
+        {
+            "name": "HTSeq",
+            "version": "0.6",
+            "path": "",
+            "description": "Python counting tool"
+        },
+        {
+            "name": "Cufflinks",
+            "version": "v1.2",
+            "path": "mypath",
+            "description": "Cufflinks goes with the Tophat suite..."
+        }
+    ]
+};
+
+var WGS = {
+    "mapping": [
+        {
+            "name": "BWA",
+            "version": "1.0",
+            "path": "",
+            "description": "BWA works..."
+        },
+        {
+            "name": "Other",
+            "version": "",
+            "path": "",
+            "description": "Other WGS mapper"
+        }
+    ],
+    "variant": [
+        {
+            "name": "VCF",
+            "version": "2",
+            "path": "asdasd",
+            "description": "asdasd"
+        },
+        {
+            "name": "No idea",
+            "version": "v2",
+            "path": "",
+            "description": "Another variant calling tool !"
+        }
+    ]
+}
